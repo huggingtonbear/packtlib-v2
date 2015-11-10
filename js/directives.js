8e096dbcfc6ac2a38c06da6e4bc7da5444e2055a
huggingtonbear <oli.huggins@gmail.com>
2015-11-10 15:19:21 +0000

    Change Directives to Test

--- js/directives.js
+++ js/directives.js
@@ -36,7 +36,7 @@ function pageTitle($rootScope, $timeout) {
         link: function(scope, element) {
             var listener = function(event, toState, toParams, fromState, fromParams) {
                 // Default title - load on Dashboard 1
-                var title = 'PacktLib | The Future today';
+                var title = 'PacktLib | The Future now';
                 // Create your own title pattern
                 if (toState.data && toState.data.pageTitle) title = 'PacktLib | ' + toState.data.pageTitle;
                 $timeout(function() {
